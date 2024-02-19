3776858e8ed2136624b3a83b84e7a2e3557c34f8
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-02-19 10:19:59 -0600

    Adding EmployeeService

---
+++ Service/EmployeeService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git.GitHub.Service
+{
+    public class EmployeeService
+    {
+        
+    }
+}
