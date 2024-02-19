022e04ffd5d44de7404e0e252294e6b6860a4ec3
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-02-19 12:00:41 -0600

    Adding Other ConsoleLog to Programs.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections.Immutable;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Adding Services");
