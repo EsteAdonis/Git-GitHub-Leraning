787394aa9a047c0f24b8b8fdb18d8461e6bc9ec6
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-02-20 08:23:15 -0600

    Adding HttpGet to UserController

--- Controller/UsersController.cs
+++ Controller/UsersController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Git.GitHub.Controller
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
-    {
-        
-    }
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UsersController : ControllerBase
+	{
+		private readonly IConfiguration _config;
+		public UsersController(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		public ActionResult GetTotalCount()
+		{
+			return Ok(10);
+		}
+	}
 }
