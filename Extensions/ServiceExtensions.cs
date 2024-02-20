0262da05eb995c8b2c610b5e46f5321716087f58
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-02-20 08:10:26 -0600

    ServiceExtensions Added

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git.GitHub.Extensions
+{
+    public class ServiceExtensions
+    {
+        
+    }
+}
